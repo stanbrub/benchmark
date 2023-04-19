66a73a676ce8781059351964486c17dc71a40ccc
Stan Brubaker <120737309+stanbrub@users.noreply.github.com>
2023-04-19 12:51:34 -0600

    Minor test name fix (#89)

--- src/it/java/io/deephaven/benchmark/tests/standard/updateby/EmaTimeDecayTest.java
+++ src/it/java/io/deephaven/benchmark/tests/standard/updateby/EmaTimeDecayTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.*;
 import io.deephaven.benchmark.tests.standard.StandardTestRunner;
 
 /**
- * Standard tests for the updateBy table operation. Calculates a tick-based exponential moving average for specified
+ * Standard tests for the updateBy table operation. Calculates a time-based exponential moving average for specified
  * columns and places the result into a new column for each row.
  */
 public class EmaTimeDecayTest {
@@ -33,14 +33,14 @@ public class EmaTimeDecayTest {
     @Test
     public void emaTimeDecay2GroupsInt() {
         var q = "timed.update_by(ops=ema_time_decay(ts_col='timestamp', time_scale='00:00:02', cols=['X=int5']), by=['str100','str150'])";
-        runner.test("EmaTickDecay- 2 Groups 15K Unique Combos 1 Col Int", runner.scaleRowCount, q, "str100", "str150",
+        runner.test("EmaTimeDecay- 2 Groups 15K Unique Combos 1 Col Int", runner.scaleRowCount, q, "str100", "str150",
                 "int5", "timestamp");
     }
     
     @Test
     public void emaTimeDecay2GroupsFloat() {
         var q = "timed.update_by(ops=ema_time_decay(ts_col='timestamp', time_scale='00:00:02', cols=['X=float5']), by=['str100','str150'])";
-        runner.test("EmaTickDecay- 2 Groups 15K Unique Combos 1 Col Float", runner.scaleRowCount, q, "str100", "str150",
+        runner.test("EmaTimeDecay- 2 Groups 15K Unique Combos 1 Col Float", runner.scaleRowCount, q, "str100", "str150",
                 "float5", "timestamp");
     }
 
