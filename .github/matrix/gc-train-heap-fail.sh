19b229e6928575bffd6cb6fbd94acea71e1a94d0
stanbrub <stanbrubaker@illumon.com>
2026-07-13 17:26:53 -0600

    Course grained heap fail static

--- .github/matrix/gc-train-heap-fail.sh
+++ .github/matrix/gc-train-heap-fail.sh
@@ -1,19 +1,17 @@
-# Sweep heap size from 48g to 4g to find failure points per GC and test class
+# Sweep heap size to find failure points per GC and test class
 # TSV columns: run_label, docker_image, test_package, test_class_list,
 #              test_iterations, scale_row_count, distribution, config_options
-EXPECTED_COMBOS=68
 
 IMG=ghcr.io/stanbrub/server:jvm25
 PKG=io.deephaven.benchmark.tests.train
-CLS='*Train'
 ITERS=1
 ROWSM=1
 DIST=random
-MAX_HEAP=48
-MIN_HEAP=16
+STEP=4
+MODE=Static  # Static+Inc
 
-BASE="-XX:+UseStringDeduplication -XX:+UseCompactObjectHeaders -DServerStateTracker.reportIntervalMillis=1000"
-CYCLE="-DPeriodicUpdateGraph.targetCycleDurationMillis=1000 -Dtrain.incLoadTarget=1.00 -Dtrain.staticInc=Static+Inc"
+BASE="-XX:+AlwaysPreTouch -XX:+UseTransparentHugePages -XX:+UseStringDeduplication -XX:+UseCompactObjectHeaders -DServerStateTracker.reportIntervalMillis=1000"
+CYCLE="-DPeriodicUpdateGraph.targetCycleDurationMillis=1000 -Dtrain.incLoadTarget=1.00 -Dtrain.staticInc=${MODE}"
 
 declare -A GCS=(
   [g1gc]="-XX:+UseG1GC"
@@ -22,10 +20,39 @@ declare -A GCS=(
   [para]="-XX:-UseG1GC -XX:+UseParallelGC"
 )
 
-for gc in g1gc zgc shen para; do
-  for h in $(seq ${MAX_HEAP} -2 ${MIN_HEAP}); do
+# Per-class, per-GC heap ranges: "class  gc  min  max"
+COMBOS=(
+  "FilterTrain       g1gc  16  48"
+  "FilterTrain       zgc   16  48"
+  "FilterTrain       shen  16  48"
+  "FilterTrain       para  16  48"
+  "FormulaTrain      g1gc  16  48"
+  "FormulaTrain      zgc   16  48"
+  "FormulaTrain      shen  16  48"
+  "FormulaTrain      para  16  48"
+  "NaturalJoinTrain  g1gc  16  48"
+  "NaturalJoinTrain  zgc   16  48"
+  "NaturalJoinTrain  shen  16  48"
+  "NaturalJoinTrain  para  16  48"
+  "AggByTrain        g1gc  16  48"
+  "AggByTrain        zgc   16  48"
+  "AggByTrain        shen  16  48"
+  "AggByTrain        para  16  48"
+  "OrderedTrain      g1gc  16  48"
+  "OrderedTrain      zgc   16  48"
+  "OrderedTrain      shen  16  48"
+  "OrderedTrain      para  16  48"
+  "UpdateByTrain     g1gc  16  48"
+  "UpdateByTrain     zgc   16  48"
+  "UpdateByTrain     shen  16  48"
+  "UpdateByTrain     para  16  48"
+)
+
+for combo in "${COMBOS[@]}"; do
+  read -r cls gc min_heap max_heap <<< "$combo"
+  for h in $(seq ${max_heap} -${STEP} ${min_heap}); do
     opts="${GCS[$gc]} -Xms${h}g -Xmx${h}g $BASE $CYCLE"
-    echo -e "gc_${gc}_1000_h${h}_j25\t$IMG\t$PKG\t$CLS\t$ITERS\t$ROWSM\t$DIST\t$opts"
+    echo -e "gc_${gc}_${cls}_h${h}_j25\t$IMG\t$PKG\t*${cls}\t$ITERS\t$ROWSM\t$DIST\t$opts"
   done
 done
 
